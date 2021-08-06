3e6a4484ffdb78e01c73ef742bdb660a228aa3cd
Anthony-J-Garot <tone.garot@gmail.com>
2021-08-06 14:29:25 -0700

    Uses useState to update the page w/ latest subscription transaction

--- src/pages/SubscribeSnippet/index.js
+++ src/pages/SubscribeSnippet/index.js
@@ -1,33 +1,51 @@
 import './index.css';
-import {gql, useSubscription,} from "@apollo/client";
-import * as Constants from '../../ApolloClient.js';
+import {gql, useSubscription, useApolloClient} from "@apollo/client";
+import {useState} from "react";
 
 /*
  * Define this page component
  */
 const SubscribeSnippet = () => {
 
-	// const [subscribe, {data: {onSnippetNoGroup}, loading}] = useSubscription(SNIPPET_NOGROUP_SUBSCRIPTION, {
-	const {loading, data, error} = useSubscription(SNIPPET_NOGROUP_SUBSCRIPTION, {});
+	const [state, setState] = useState({
+		title: '. . . ',
+		body: '. . . ',
+		private: true
+	});
+
+	const {loading, data, error} = useSubscription(SNIPPET_NOGROUP_SUBSCRIPTION, {
+		variables: {},
+		onSubscriptionData: (data) => {
+			console.log("SUBSCRIPTION: data is ", data);
+			setState(data.subscriptionData.data.onSnippetNoGroup.snippet);
+		},
+		fetchPolicy: "network-only", // not really sure what a caching option means in the context of a subscription
+		client: useApolloClient(), // unneeded, but leaving as a placeholder to show that I could specify a different one
+	});
 
 	if (loading) {
-		console.log("loading");
+		// console.log("loading");
 	}
 
 	if (error) {
 		console.log(error);
 	}
 
-	if (data) {
-		console.log(data);
-	}
+	return (
+		<Transaction {...state} />
+	)
+}
+
+const Transaction = (state) => {
+	//console.log("State ", state);
 
 	return (
 		<div>
-			<p className="App-page-title">Subscribe Snippet</p>
-			<p>{data && data.onSnippetNoGroup.sender}</p>
+			<p>Snippet.title: <span id="title">{state.title}</span></p>
+			<p>Snippet.body: <span id="body">{state.body}</span></p>
+			<p>Snippet.private: <span id="private">{state.private}</span></p>
 		</div>
-	);
+	)
 }
 
 /*
@@ -45,6 +63,7 @@ subscription subNoGroup {
       title
       private
       created
+      body
     }
     ok
   }

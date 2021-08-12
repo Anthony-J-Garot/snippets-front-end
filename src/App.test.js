d45bba7287466d5c46f7eb8b56e4fc9f43204ce4
Anthony-J-Garot <tone.garot@gmail.com>
2021-08-12 15:51:36 -0700

    Fixes the initial unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import {ApolloProvider} from "@apollo/client";
+import client from "./ApolloClient";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+// Need some goop around <App /> because of routes
+test('renders App', () => {
+  render(
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
+  );
+
+  // Basically old school screen scraping
+  const linkElement = screen.getByText(/Snippets Front-end App/i);
   expect(linkElement).toBeInTheDocument();
 });

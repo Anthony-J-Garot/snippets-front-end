1fa95a87fbed51f8bc87190c1474536339e33495
Anthony-J-Garot <tone.garot@gmail.com>
2021-08-20 16:02:45 -0700

    First attempt towards cleanup of subscription tsx

--- src/pages/SubscribeSnippet/index.tsx
+++ src/pages/SubscribeSnippet/index.tsx
@@ -2,21 +2,33 @@ import React, {ReactElement} from 'react';
 import './index.css';
 import {gql, useSubscription, useApolloClient} from '@apollo/client';
 import {useState} from 'react';
-import noticesStore from '../../Observables/noticesStore.ts';
+import noticesStore from '../../Observables/noticesStore';
 
-const transactions = [];
+interface IFeedItem {
+  sender: string,
+  snippet: {
+    id: string,
+    title: string,
+    body: string,
+    private: boolean,
+    created: string
+  }
+}
+
+const transactions: IFeedItem[] = [];
 
 // Placeholder values as I figure out where these fields should go on the page.
-transactions.push({
+const placeholder: IFeedItem = {
   sender: 'Books R Us',
   snippet: {
-    id: '0',
+    id: '0',    // placeholder can have a 0 id
     title: 'Calahan\'s Crosstime Saloon',
     body: 'Lighter fare. I read this in evenings that I was fatigued . . ',
     private: false,
     created: '2021-08-04T15:49:43.000000-07:00'
   }
-});
+};
+transactions.push(placeholder);
 
 /*
  * Define this page component
@@ -55,36 +67,62 @@ const SubscribeSnippet: React.FC = (): ReactElement => {
   return (
     <div id="feed">
       <p className="App-page-title">Real-time Subscription Feed</p>
-      <Transaction feedItems={state} />
+      <Transaction {...state} />
     </div>
   );
 };
 
-const Transaction = ({feedItems}) => {
-  if (!feedItems) return (null);
+const Transaction = (feedItems:IFeedItem[]) => {
+//const Transaction = (feedItems) => {
+  console.log(feedItems);
+
+  // Nothing to see here; move along . . .
+  if (!feedItems) return null;
+
   const items = feedItems.transactions;		// shorthand
 
   console.log('Mapping Feed Transactions (' + (items.length) + ')', items);
 
-  const SubscriptionFeed = items.map((item, index) =>
-    <li key={index}>
-      <div className="sender">{item.sender}</div>
-      <div className="row">
-        <div className="col1">
-          {item.snippet.id}
-        </div>
-        <div className="col2">
-          {item.snippet.title}
-        </div>
-        <div className="col3">
-          {item.snippet.body}
-        </div>
-        <div className="col4">
-          {item.snippet.private}
-        </div>
-      </div>
-    </li>
-  );
+  const SubscriptionFeed = items.map((item:IFeedItem, index:number) => {
+
+    if (item.snippet !== null) {
+      return (
+        <li key={index}>
+          <div className="sender">{item.sender}</div>
+          <div className="row">
+            <div className="col1">
+              {item.snippet.id}
+            </div>
+            <div className="col2">
+              {item.snippet.title}
+            </div>
+            <div className="col3">
+              {item.snippet.body}
+            </div>
+            <div className="col4">
+              {item.snippet.private}
+            </div>
+          </div>
+        </li>
+      );
+    } else {
+      return (
+        <li key={index}>
+          <div className="sender">{item.sender}</div>
+          <div className="row">
+            <div className="col1">
+            </div>
+            <div className="col2">
+            </div>
+            <div className="col3">
+            </div>
+            <div className="col4">
+            </div>
+          </div>
+        </li>
+      );
+    }
+  });
 
   return (
     <ul>

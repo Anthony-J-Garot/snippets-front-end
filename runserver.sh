59356775e7364d8574242d02ffed29135e5260ce
Anthony-J-Garot <tone.garot@gmail.com>
2021-08-25 08:58:02 -0700

    Adds SKIP_PREFLIGHT_CHECK to runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -3,9 +3,11 @@
 # I can choose a different port this way
 export PORT=3000
 
-# This didn't work to eliminate the myriad warning messages. Wish it did.
-# export SKIP_PREFLIGHT_CHECK=eslint
-# export SKIP_PREFLIGHT_CHECK=true
+# I have a dependency issue with "jest": "26.6.0" vs. version: 27.0.6.
+# Well, testing spec-definitions doesn't work with the older jest.
+# This gets rid of the long warning message when trying to start the
+# server.
+export SKIP_PREFLIGHT_CHECK=true
 
 # Start up `react-scripts start`
 yarn start

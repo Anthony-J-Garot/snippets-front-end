ee9f6ea3acc00617d23c22807e755c4804e0b504
Anthony-J-Garot <tone.garot@gmail.com>
2021-08-30 14:35:48 -0700

    Sunders SignOnOff component from Navbar.tsx code

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,66 +1,6 @@
-import React, {ReactElement, useEffect, useState} from 'react';
+import React, {ReactElement} from 'react';
 import {Link} from 'react-router-dom';
-import {clearAuthToken, getAuthToken} from './authentication';
-import userStore from './Observables/userStore';
-import {ANONYMOUS_USER} from './constants';
-
-const defaultUsername = {username: ANONYMOUS_USER};
-
-// Logs off the user
-const signOffUser = (): void => {
-  clearAuthToken();
-  userStore.setUser({username: ANONYMOUS_USER});  // Clear it
-};
-
-// The <SignOnOff /> component renders based upon current authentication state.
-const SignOnOff = (): ReactElement => {
-
-  // Set-up the authToken state
-  const [authToken, setAuthToken] = useState(getAuthToken());
-
-  // Set-up the username state
-  const [username, setUsername] = useState(defaultUsername);
-
-  useEffect(() => {
-    // Subscribe to the username because that will be the dependency for the next effect
-    userStore.subscribe(setUsername);
-  }, []);
-
-  // Next cycle, this gets run. (After the component is rendered.)
-  // Is only called once.
-  // deps = what it triggers from.
-  useEffect(() => {
-    // Every time the username changes, I want to verify that the user is still authenticated.
-    const tempToken = getAuthToken();
-
-    // A change in the token triggers the whole component to re-render.
-    if (tempToken !== authToken) {
-      setAuthToken(tempToken);
-    }
-  }, [username]);
-
-  // Shows up the second time-through.
-  if (authToken !== '') {
-    return (
-      <div>
-        {Username(username.username)}
-        <p><a href='#' onClick={signOffUser}>Sign off</a></p>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        {Username(username.username)}
-        <p><Link to="/user">Sign on</Link></p>
-      </div>
-    );
-  }
-};
-
-// Show the username in the Navbar
-const Username = (username: string): ReactElement => (
-  <p><span className='username-label'>User: </span><span className='username'>{username}</span></p>
-);
+import SignOnOff from './pages/User/SignOnOff';
 
 
 /*

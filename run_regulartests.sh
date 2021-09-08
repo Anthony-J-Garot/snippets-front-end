c81eadde86733a1e2cc590509383c3a87139e2f6
Anthony-J-Garot <tone.garot@gmail.com>
2021-09-08 09:03:04 -0700

    Updates test runner scripts

--- run_regulartests.sh
+++ run_regulartests.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-YARN=~/.nvm/versions/node/v14.17.5/bin/yarn
-SRC="./src/"
+# Example to run just the tests in a single file or directory:
+# 	$ ./run_regulartests.sh src/pages/SubscribeSnippet/
+# which is the same as issuing:
+# 	$ yarn test src/pages/SubscribeSnippet/
 
-DIR=$1
-if [[ "$DIR" == "" ]]; then
-	DIR=$SRC
+YARN=~/.nvm/versions/node/v14.17.5/bin/yarn
+SRC="./src/"		# This is the default directory for regular tests
+
+# Allow for a passed directory or single test file
+DIR_OR_TEST_FILE=$1
+if [[ "$DIR_OR_TEST_FILE" == "" ]]; then
+	DIR_OR_TEST_FILE=$SRC
 fi
 
-$YARN jest --detectOpenHandles --watch --color $DIR
+# Now run the command
+$YARN jest --detectOpenHandles --watch --color $DIR_OR_TEST_FILE
--- run_specifications.sh
+++ run_specifications.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
 # This runs all jest-cucumber specifications.
-# It doesn't run standard unit tests.
+# It doesn't run the "standard" unit tests in the /src/ directory.
+#
+# I needed to add a "jest" section to package.json to get this to work.
+#
+# Usage:
+#	$ ./run_specification.sh [DIR_OR_TEST_FILE] [OUTPUT_FILE]
+# Example:
+#	$ ./run_specifications.sh specs/step-definitions/CreateSnippet.steps.tsx /tmp/junk
 
 YARN=~/.nvm/versions/node/v14.17.5/bin/yarn
-SPECS_DIR="specs/step-definitions"
+SPECS_DIR="specs/step-definitions"		# default dir for jest-cucumber specs.
 
-DIR=$1
-if [[ "$DIR" == "" ]]; then
-	DIR=$SPECS_DIR
+# Allow specification of directory or single test file from command line
+DIR_OR_TEST_FILE=$1
+if [[ "$DIR_OR_TEST_FILE" == "" ]]; then
+	DIR_OR_TEST_FILE=$SPECS_DIR
 fi
 
-$YARN jest --detectOpenHandles --watch --color $DIR
+# The --color option is great for console, but it's not so good when directing to a file.
+# Actually, most options aren't good when directing to a file.
+OUTPUT_FILE=$2
+if [[ "$OUTPUT_FILE" == "" ]]; then
+	echo "Sending output to console"
+	CMD="$YARN jest --detectOpenHandles --watch --color $DIR_OR_TEST_FILE"
+else
+	echo "Sending output to $OUTPUT_FILE"
+	CMD="$YARN jest $DIR_OR_TEST_FILE > $OUTPUT_FILE"
+fi
+echo $CMD
+eval $CMD

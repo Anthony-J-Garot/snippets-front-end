e5114109309c94b14460eb02d133a9c5e09e0160
Anthony-J-Garot <tone.garot@gmail.com>
2021-09-13 07:32:33 -0700

    Cleans up NavBar for greater clarity

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,25 +3,24 @@ import {Link} from 'react-router-dom';
 import SignOnOff from './pages/User/SignOnOff';
 
 
-/*
- * The component I want to render
- */
 const Navbar = (): ReactElement => {
 
   return (
     <div className="navbar">
-      <p><Link to="/">Home</Link></p>
-      <p><Link to="/snippet/my">My Snippets</Link></p>
-      <p><Link to="/snippet/all">All Snippets</Link></p>
-      <p><Link to="/snippet/create">Create Snippet</Link></p>
-      <p><Link to="/snippet/subscribe">Subscribe</Link></p>
+      <p className='navbar-header'>Main Links</p>
 
+      <p><Link to="/">Home</Link></p>
+      <p><Link to="/snippet/my">List My Snippets</Link></p>
+      <p><Link to="/snippet/create">Create New Snippet</Link></p>
+      <p><Link to="/snippet/subscribe">Subscribe To Feed</Link></p>
+      <SignOnOff />
+
+      <hr />
+      <p className='navbar-header'>Developer Links</p>
+      <p><Link to="/snippet/all">List All Snippets</Link></p>
       {/* A link to the Playground on the API side. */}
       <p><a href="http://192.168.2.99:4000/graphql/" target="_blank" rel="noreferrer">GraphiQL Playground</a></p>
-      <br />
-      <br />
-      <br />
-      <SignOnOff />
+      <p><a href="http://192.168.2.99:4000/snippets/" target="_blank" rel="noreferrer">Django API GUI</a></p>
     </div>
   );
 };
